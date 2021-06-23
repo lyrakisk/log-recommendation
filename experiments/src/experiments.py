0b18d74f46a6b439b607d3d8497534ab2e60238f
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2021-06-23 21:14:10 +0300

    Added code for the experiment of RQ3.

--- experiments/src/experiments.py
+++ experiments/src/experiments.py
@@ -8,6 +8,7 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 # Create classifiers
 classifiers = {}
@@ -50,7 +51,6 @@ if __name__ == "__main__":
 	# Answering RQ1: Classifiers' performance
 	results_rq1 = pd.DataFrame(columns = ['Acc', 'BA', 'AUC', 'Precision', 'Recall', 'F1', 'Time'])
 	for name, clf in classifiers.items(): 
-	    print("Validating " + name)
 	    temp = train_and_evaluate_classifier(dataset, labels, clf, name)
 	    results_rq1 = results_rq1.append(temp)
 		
@@ -77,3 +77,39 @@ if __name__ == "__main__":
 
 
 	# Answering RQ3: Size vs Accuracy
+	num_bins = 100
+	step = 10
+
+	bin_names = []
+	for i in range(step, num_bins + 1, step):
+	    bin_names.append(str(i - step + 1) + "-" + str(i))
+	train_x, test_x, train_y, test_y = train_test_split(dataset, labels, test_size=0.1, random_state=42)
+	predictions = {}
+
+	for name, clf in classifiers.items(): 
+	    clf.fit(train_x, train_y)
+	    predictions[name] = clf.predict(test_x)
+	sizes = [x.count_nonzero() for x in dataset if x.count_nonzero() <= num_bins]
+	minimum = min(sizes)
+	maximum = max(sizes)
+
+	results_rq3 = pd.DataFrame(columns = bin_names)
+	for k, v in predictions.items():
+		scores = [] 
+		temp = {}
+		for i in range(step, num_bins + 1, step):
+			y_pred = []
+			y_true = []
+			for j in range(test_x.shape[0]):
+				l = test_x[j].count_nonzero()
+				if (l <= i and l > i - step):
+					y_pred.append(v[j])
+					y_true.append(test_y[j])
+			scores.append(f1_score(y_true, y_pred, zero_division=1))
+			temp[bin_names[i // step - 1]] = f1_score(y_true, y_pred, zero_division=1)
+		temp = pd.DataFrame(temp, index = [k])
+		results_rq3 = results_rq3.append(temp)
+	results_rq3.index.name = 'Classifier'
+
+	results_rq3.to_csv(results_destination_folder + "/size_accuracy.csv")
+	print("Experiments finished!")
